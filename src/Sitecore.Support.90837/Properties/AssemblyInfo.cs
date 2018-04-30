a6c6d76224ed0f1c6c232073bfb5d19718e72e36
DK\IRO <iro@sitecore.net>
2018-04-30 15:39:47 +0300

    Initial

---
+++ src/Sitecore.Support.90837/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.90837")]
+[assembly: AssemblyProduct("Sitecore.Support.90837")]
+[assembly: ComVisible(false)]
